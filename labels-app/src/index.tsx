2abc4fa1360f352ae9e63279dff2556b994c67bd
hkrnnkw <4901108@gmail.com>
2020-11-28 15:20:09 +0900

    ダブルクォーてションをシングルに

--- labels-app/src/index.tsx
+++ labels-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
-import { Provider } from "react-redux";
-import store from "./stores/index";
+import { Provider } from 'react-redux';
+import store from './stores/index';
 import App from './App';
 
 ReactDOM.render(
