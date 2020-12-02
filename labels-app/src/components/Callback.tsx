a0b725a66809bff7f44b0b294a5de146623667c8
hkrnnkw <4901108@gmail.com>
2020-12-02 17:54:13 +0900

    signInWithCustomTokenを非同期に

--- labels-app/src/components/Callback.tsx
+++ labels-app/src/components/Callback.tsx
@@ -36,31 +36,30 @@ const Callback: FC = () => {
     };
 
     // Firebaseログイン
-    const signInWithCustomToken = (customToken : string) => {
-        auth.signInWithCustomToken(customToken)
-            .then(response => {
-                const user = response.user;
-                if (!user) {
-                    history.push('/error');
-                    return;
-                }
-                const newState: UserState = {
-                    uid: user.uid,
-                    signedIn: true,
-                    refreshToken: user.refreshToken,
-                    displayName: user.displayName || user.uid,
-                    // TODO emailがnullなら、要求する？
-                    email: user.email || '',
-                    photoURL: user.photoURL,
-                    emailVerified: user.emailVerified,
-                };
-                dispatch(setUserProfile(newState));
-                history.push('/');
-            })
-            .catch(error => {
-                console.log(`カスタムトークンによるログインでエラーが発生しました：${error.message} (ErrorCode ${error.code})`);
-                history.push('/error');
-            });
+    const signInWithCustomToken = async (customToken : string) => {
+        try {
+            const response: firebase.auth.UserCredential = await auth.signInWithCustomToken(customToken);
+            const user = response.user;
+            if (!user) {
+                history.push('/notfound');
+                return;
+            }
+            const newState: UserState = {
+                uid: user.uid,
+                signedIn: true,
+                refreshToken: user.refreshToken,
+                displayName: user.displayName || user.uid,
+                // TODO emailがnullなら、要求する？
+                email: user.email || '',
+                photoURL: user.photoURL,
+                emailVerified: user.emailVerified,
+            };
+            await dispatch(setUserProfile(newState));
+            history.push('/');
+        } catch (err) {
+            console.log(`カスタムトークンによるログインでエラーが発生しました：${err.message}`);
+            history.push('/notfound');
+        }
     };
 
     // CloudFunctions経由で、Spotifyのアクセストークン認証
@@ -70,9 +69,10 @@ const Callback: FC = () => {
         const spotifyToken: firebase.functions.HttpsCallable = f.httpsCallable('spotifyToken');
         const res: SpotifyTokenResponse = await spotifyToken(params);
         const customToken: string | null = res.data;
-        if (customToken && customToken.length) signInWithCustomToken(customToken);
+        if (customToken && customToken.length) signInWithCustomToken(customToken).catch(err => console.log(err));
     };
 
+    // TODO? https://qiita.com/zaburo/items/92920fa955bdb890c52e
     // const a = (refreshToken: string) => {
     //     const params = new URLSearchParams();
     //     params.append('grant_type', 'refresh_token');
