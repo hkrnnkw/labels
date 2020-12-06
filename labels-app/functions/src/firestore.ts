f72af792cbf8edeacc811e64d8f257f4649d4202
hkrnnkw <4901108@gmail.com>
2020-12-06 16:54:02 +0900

    [functions] firestore.ts リファクタリング

--- labels-app/functions/src/firestore.ts
+++ labels-app/functions/src/firestore.ts
@@ -5,41 +5,34 @@ export const manageUser = async (accessToken: string, spotifyID: string, display
     photoURL: string | null, email: string): Promise<string> => {
         
     // Spotifyのアクセストークンを保存
-    const databaseTask = admin.firestore()
-        .collection('users').doc(spotifyID).set({
-            spotifyToken: accessToken,
-            displayName: displayName,
-            photoURL: photoURL,
-            email: email,
-        });
+    const databaseTask = admin.firestore().collection('users').doc(spotifyID).set({
+        spotifyToken: accessToken,
+        displayName: displayName,
+        photoURL: photoURL,
+        email: email,
+    });
 
     // アカウント更新／作成
-    const userAccountTask = admin.auth()
-        .updateUser(spotifyID, {
-            displayName: displayName,
-            email: email,
-        })
-        .catch((error) => {
-            // ユーザが存在しなければ作成する
-            if (error.code === 'auth/user-not-found') {
-                return admin.auth().createUser({
-                    uid: spotifyID,
-                    displayName: displayName,
-                    email: email,
-                });
-            }
-            throw error;
-        });
+    const userAccountTask = admin.auth().updateUser(spotifyID, {
+        displayName: displayName,
+        email: email,
+    }).catch((error) => {
+        // ユーザが存在しなければ作成する
+        if (error.code === 'auth/user-not-found') {
+            return admin.auth().createUser({
+                uid: spotifyID,
+                displayName: displayName,
+                email: email,
+            });
+        }
+        throw error;
+    });
 
     await Promise.all([userAccountTask, databaseTask]);
-    const result: string = await admin.auth().createCustomToken(spotifyID)
-        .then(customToken => {
-            console.log(`${spotifyID}のカスタムトークンを作成しました：${customToken}`);
-            return customToken;
-        })
-        .catch(error => {
-            console.log('カスタムトークン作成中にエラーが発生しました：', error);
+    const customToken: string = await admin.auth().createCustomToken(spotifyID)
+        .catch(() => {
             return '';
         });
-    return result;
+    console.log(`${spotifyID}のカスタムトークンを作成しました：${customToken}`);
+    return customToken;
 }
