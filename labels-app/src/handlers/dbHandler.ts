8a835f55c3868e37aa23b488e48fa3da533f72ca
hkrnnkw <4901108@gmail.com>
2020-12-30 13:52:24 +0900

    firestoreからリフレッシュトークンを取得できた

--- labels-app/src/handlers/dbHandler.ts
+++ labels-app/src/handlers/dbHandler.ts
@@ -5,9 +5,7 @@ export const takeOutSpotifyRefreshTokenFromFirestore = async (uid: string): Prom
     try {
         const doc = await db.collection("users").doc(uid).get();
         const data = doc.data();
-        // TODO DBルールを見直し、以下のコードの動作確認
         if (!data) return null;
-        console.log(`データ：${data}`);
         const refreshToken: string = data.spotifyRefreshToken;
         return refreshToken;
     } catch (err) {
