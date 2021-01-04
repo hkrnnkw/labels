79920a77c20bfd8592bd35e329035c05f49234e6
hkrnnkw <4901108@gmail.com>
2021-01-04 23:41:48 +0900

    Account.tsx フォロー中のレーベル一覧を表示

--- labels-app/src/components/Account.tsx
+++ labels-app/src/components/Account.tsx
@@ -1,14 +1,32 @@
 import React, { FC } from 'react';
 import { withRouter } from 'react-router';
+import { useSelector } from 'react-redux';
+import { Link as RouterLink } from 'react-router-dom';
+import { RootState } from '../stores/index';
 import {
-    Typography,
+    Typography, Link, List, ListItem,
 } from '@material-ui/core';
+import { label as labelPath } from '../utils/paths';
 
 const Account: FC = () => {
+    const { followingLabels } = useSelector((rootState: RootState) => rootState.albums);
 
     return (
         <div>
             <Typography>マイページ</Typography>
+            {followingLabels.length > 0 ?
+                <List>
+                    {followingLabels.map(label => {
+                        return (
+                            <Link component={RouterLink} to={{ pathname: `${labelPath}/${label}`, state: { label: label } }}>
+                                <ListItem>{label}</ListItem>
+                            </Link>
+                        )
+                    })}
+                </List>
+                :
+                <Typography>フォローしているレーベルがまだありません</Typography>
+            }
         </div>
     )
 };
