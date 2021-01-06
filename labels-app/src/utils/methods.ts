20e7091fd9846a790cf3ddb95d91c76efee104d9
hkrnnkw <4901108@gmail.com>
2021-01-06 19:08:37 +0900

    sortHandler()を通してソートを実行する

--- labels-app/src/utils/methods.ts
+++ labels-app/src/utils/methods.ts
@@ -1,7 +1,28 @@
 import { FavLabel, SortOrder } from "./types";
 
+export const sortHandler = (array: FavLabel[], order: SortOrder): FavLabel[] => {
+    switch (order) {
+        case 'DateAsc': return sortDate(array);
+        case 'DateDesc': return sortDate(array, true);
+        case 'NameAsc': return sortLabelName(array);
+        case 'NameDesc': return sortLabelName(array, true);
+        default: return [];
+    }
+};
+
+// フォローした日時でソート
+const sortDate = (array: FavLabel[], desc: boolean = false): FavLabel[] => {
+    return array.sort((a: FavLabel, b: FavLabel) => {
+        const aa = a.date, bb = b.date;
+        return desc ?
+            (aa > bb ? -1 : aa < bb ? 1 : 0)    // 降順
+            :
+            (aa < bb ? -1 : aa > bb ? 1 : 0);   // 昇順
+    });
+};
+
 // （大文字・小文字を区別せず）レーベル名でソート
-export const sortLabelName = (array: FavLabel[], desc: boolean = false): FavLabel[] => {
+const sortLabelName = (array: FavLabel[], desc: boolean = false): FavLabel[] => {
     return array.sort((a: FavLabel, b: FavLabel) => {
         const aa = a.labelName.toLowerCase(), bb = b.labelName.toLowerCase();
         return desc ?
