fdf4e5f4b795963a82c4b93c1bc57682695ae296
hkrnnkw <4901108@gmail.com>
2021-01-12 23:23:13 +0900

    .scssは使わないので削除

--- labels-app/src/index.tsx
+++ labels-app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
 import { Provider } from 'react-redux';
 import store from './stores/index';
 import App from './App';
