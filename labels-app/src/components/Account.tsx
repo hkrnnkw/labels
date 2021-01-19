5f76576407dc18bdf4d82ae6cc76d0fed6bd5e4f
hkrnnkw <4901108@gmail.com>
2021-01-20 01:12:04 +0900

    マイページにソートを実装

--- labels-app/src/components/Account.tsx
+++ labels-app/src/components/Account.tsx
@@ -1,17 +1,36 @@
 import React, { FC } from 'react';
 import { withRouter } from 'react-router';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { auth } from '../firebase';
 import { RootState } from '../stores/index';
+import { setSortOrder } from '../stores/albums';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {
     Typography, Link, List, ListItem, Button, 
 } from '@material-ui/core';
 import { label as labelPath } from '../utils/paths';
-import { LabelEntry } from '../utils/types';
-import { sortHandler } from '../handlers/sortHandler';
+import { LabelEntry, SortOrder } from '../utils/types';
+import { DATE_ASC, DATE_DESC, NAME_ASC, NAME_DESC, sortHandler } from '../handlers/sortHandler';
+import { CustomSwipeableDrawer } from './custom/CustomSwipeableDrawer';
+
+const ambiguousStyles = makeStyles((theme: Theme) => createStyles({
+    contentClass: {
+        minHeight: '100vh',
+    },
+    root: {
+        backgroundColor: theme.palette.background.default,
+    },
+    '@media (min-width: 960px)': {
+        contentClass: {
+            display: 'flex',
+        },
+    },
+}));
 
 const Account: FC = () => {
+    const dispatch = useDispatch();
+    const classes = ambiguousStyles();
     const { home, sortOrder } = useSelector((rootState: RootState) => rootState.albums);
     const sorted: LabelEntry[] = sortHandler(home, sortOrder);
     const filtered = sorted.filter(([name, fav]) => fav.date > 0);
@@ -19,9 +38,25 @@ const Account: FC = () => {
     // サインアウト
     const signOut = async () => await auth.signOut();
 
+    // レーベルの並び替え
+    const sortOrderList = [DATE_DESC, DATE_ASC, NAME_ASC, NAME_DESC];
+    const handleSortOrder = (option: string) => {
+        const getSortOrder = (): SortOrder => {
+            switch (option) {
+                case DATE_ASC: return 'DateAsc';
+                case DATE_DESC: return 'DateDesc';
+                case NAME_ASC: return 'NameAsc';
+                case NAME_DESC: return 'NameDesc';
+                default: return null;
+            }
+        };
+        const newOrder: SortOrder = getSortOrder();
+        dispatch(setSortOrder(newOrder));
+    };
+
     return (
-        <div>
-            <Typography>マイページ</Typography>
+        <div className={classes.root}>
+            <CustomSwipeableDrawer texts={sortOrderList} action={handleSortOrder} />
             {filtered.length > 0 ?
                 <List>
                     {filtered.map(([name, fav]) => {
